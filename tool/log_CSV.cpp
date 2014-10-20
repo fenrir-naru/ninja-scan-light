9de314769dee238bae486d1dadc88a00836fb984
fenrir-naru <fenrir.naru@gmail.com>
2014-10-20 11:45:27 +0000

    Add --calendar_time option.
    git-svn-id: http://ninja-scan-light.googlecode.com/svn/trunk@212 94d2995a-3457-b8a5-7510-40b377e6a921

--- tool/log_CSV.cpp
+++ tool/log_CSV.cpp
@@ -41,6 +41,7 @@ typedef __int64 int64_t;
 #include <string>
 #include <sstream>
 #include <exception>
+#include <ctime>
 
 #define IS_LITTLE_ENDIAN 1
 #include "SylphideStream.h"
@@ -61,6 +62,13 @@ struct Options : public GlobalOptions {
   bool page_other;
   int page_P_mode, page_F_mode, page_M_mode;
   int debug_level;
+  struct {
+    bool valid;
+    time_t utc_time;
+    unsigned int itow_sec;
+  } gps_utc;
+  bool use_calendar_time;
+  int localtime_correction_in_seconds;
   
   Options() 
       : GlobalOptions(),
@@ -70,9 +78,37 @@ struct Options : public GlobalOptions {
       page_P_mode(5),
       page_F_mode(3),
       page_M_mode(0),
-      debug_level(0) {}
+      debug_level(0),
+      use_calendar_time(false), localtime_correction_in_seconds(0) {
+    gps_utc.valid = false;
+  }
   ~Options(){}
   
+  template <class T>
+  string str_time(const T &itow){
+    stringstream ss;
+    ss.precision(10);
+    if(use_calendar_time){ // year, month, mday, hour, min, sec
+      if(gps_utc.valid){
+        T interval(itow - gps_utc.itow_sec);
+        time_t interval_time(interval);
+        time_t current(gps_utc.utc_time + interval_time + localtime_correction_in_seconds);
+        tm *current_tm(gmtime(&current));
+        ss << current_tm->tm_year + 1900 << ", "
+            << current_tm->tm_mon + 1 << ", "
+            << current_tm->tm_mday << ", "
+            << current_tm->tm_hour << ", "
+            << current_tm->tm_min << ", "
+            << (interval - interval_time + current_tm->tm_sec);
+      }else{
+        ss << "0, 0, 0, 0, 0, " << itow;
+      }
+    }else{
+      ss << itow;
+    }
+    return ss.str();
+  }
+
   /**
    * コマンドに与えられた設定を読み解く
    * 
@@ -106,7 +142,20 @@ struct Options : public GlobalOptions {
       cerr << "page_other: " << (page_other ? "on" : "off") << endl;
       return true;
     }
-    if((strstr(spec, "--page=") == spec) 
+    if(strstr(spec, "--calendar_time") == spec){ // Change time outputs from gpstime[ms] to calendartime(YY,MM,DD,HH,MM,SS) formats
+      char *value(spec + strlen("--calendar_time"));
+      int correction_hr(0);
+      if(*value == '='){
+        correction_hr = atoi(value + 1); // Specify time zone by hour.
+      }
+      use_calendar_time = true;
+      localtime_correction_in_seconds = 60 * 60 * correction_hr;
+      cerr << "use_calendar_time: UTC "
+          << (correction_hr >= 0 ? '+' : '-')
+          << correction_hr << " [hr]" << endl;
+      return true;
+    }
+    if((strstr(spec, "--page=") == spec)
         && (c = *(spec + strlen("--page=")))){
       switch(c){
         case 'A': page_A = true; break;
@@ -158,12 +207,12 @@ class StreamProcessor : public SylphideProcessor<float_sylph_t> {
         if(!options.is_time_in_range(current)){return;}
         
         options.out() 
-            << (count++) << ","
-            << current << ",";
+            << (count++) << ", "
+            << options.str_time(current) << ", ";
         
         A_Observer_t::values_t values(observer.fetch_values());
         for(int i(0); i < 8; i++){
-          options.out() << values.values[i] << ",";
+          options.out() << values.values[i] << ", ";
         }
         options.out() << values.temperature << endl;
       }
@@ -183,11 +232,18 @@ class StreamProcessor : public SylphideProcessor<float_sylph_t> {
       super_t::G_Observer_t::position_acc_t position_acc;
       super_t::G_Observer_t::velocity_t velocity;
       super_t::G_Observer_t::velocity_acc_t velocity_acc;
+      time_t gpstime_zero;
       HandlerG() 
           : itow_ms_0x0102(0), itow_ms_0x0112(0),
           position(0, 0, 0), position_acc(0, 0),
           velocity(0, 0, 0), velocity_acc(0) {
-        
+        tm tm_utc;
+        tm_utc.tm_hour = tm_utc.tm_min = tm_utc.tm_sec = 0;
+        tm_utc.tm_mday = 6;
+        tm_utc.tm_mon = 0;
+        tm_utc.tm_year = 80;
+        tm_utc.tm_isdst = 0;
+        gpstime_zero = mktime(&tm_utc); // 1980/1/6 00:00:00
       }
       ~HandlerG(){}
       
@@ -218,6 +274,21 @@ class StreamProcessor : public SylphideProcessor<float_sylph_t> {
                 
                 break;
               }
+              case 0x20: {
+                if(!options.use_calendar_time){break;}
+                char buf[2];
+                observer.inspect(buf, sizeof(buf), 6 + 10);
+                if(!((unsigned char)buf[1] & 0x04)){break;}// Invalid UTC
+                char leap_seconds(buf[0]);
+                observer.inspect(buf, sizeof(buf), 6 + 8);
+                unsigned short gps_week(le_char2_2_num<unsigned short>(*buf));
+                options.gps_utc.itow_sec = (observer.fetch_ITOW_ms() / 1000);
+                options.gps_utc.utc_time = gpstime_zero
+                    + (7u * 24 * 60 * 60) * gps_week
+                    + options.gps_utc.itow_sec;
+                options.gps_utc.valid = true;
+                break;
+              }
             }
             break;
           }
@@ -231,7 +302,7 @@ class StreamProcessor : public SylphideProcessor<float_sylph_t> {
           float_sylph_t current(1E-3 * itow_ms_0x0102);
           if(!options.is_time_in_range(current)){return;}
           
-          options.out() << current << ", "
+          options.out() << options.str_time(current) << ", "
               << position.latitude << ", "
               << position.longitude << ", "
               << position.altitude << ", "
@@ -261,7 +332,7 @@ class StreamProcessor : public SylphideProcessor<float_sylph_t> {
         if(!options.is_time_in_range(current)){return;}
         
         options.out() << (count++)
-             << ", " << current;
+             << ", " << options.str_time(current);
         
         F_Observer_t::values_t values(observer.fetch_values());
         for(int i = 0; i < 8; i++){
@@ -331,8 +402,8 @@ class StreamProcessor : public SylphideProcessor<float_sylph_t> {
               coef[i] = be_char2_2_num<Uint16>(*buf);
             }
 
-            for(int i(0); i < 2; i++){
-              options.out() << current << ", " << (i - 1) << ", ";
+            for(int i(0), j(-1); i < 2; i++, j++){
+              options.out() << options.str_time(current) << ", " << j << ", ";
               char buf[2][4];
               buf[0][0] = buf[1][0] = 0;
               observer.inspect(&buf[0][1], 3, 7 + 6 * i);
@@ -369,16 +440,16 @@ class StreamProcessor : public SylphideProcessor<float_sylph_t> {
 
         switch(options.page_M_mode){
           case 1: // -atan2(y, x)した結果[deg]を表示
-            for(int i(0); i < 4; i++){
-              options.out() << current << ", "
-                   << (i - 3) << ", "
+            for(int i(0), j(-3); i < 4; i++, j++){
+              options.out() << options.str_time(current) << ", "
+                   << j << ", "
                    << rad2deg(-atan2((double)values.y[i], (double)values.x[i])) << endl;
             }
             break;
           default:
-            for(int i(0); i < 4; i++){
-              options.out() << current << ", "
-                   << (i - 3) << ", "
+            for(int i(0), j(-3); i < 4; i++, j++){
+              options.out() << options.str_time(current) << ", "
+                   << j << ", "
                    << values.x[i] << ", "
                    << values.y[i] << ", "
                    << values.z[i] << endl;
@@ -404,7 +475,7 @@ class StreamProcessor : public SylphideProcessor<float_sylph_t> {
           case 0: {
             N_Observer_t::navdata_t values(observer.fetch_navdata());
             
-            options.out() << values.itow << ", "
+            options.out() << options.str_time(values.itow) << ", "
                 << values.longitude << ", "
                 << values.latitude << ", "
                 << values.altitude << ", "
@@ -491,15 +562,16 @@ class StreamProcessor : public SylphideProcessor<float_sylph_t> {
         }
       
         switch(buffer[0]){
-#define assign_case(type, mark) \
-case mark: if(options.page_ ## type){ \
+#define assign_case_cnd(type, mark, cnd) \
+case mark: if(cnd){ \
   super_t::process_packet( \
       buffer, read_count, \
       observer_ ## type , previous_seek_next_ ## type, handler_ ## type); \
 } \
 break;
+#define assign_case(type, mark) assign_case_cnd(type, mark, options.page_ ## type)
           assign_case(A, 'A');
-          assign_case(G, 'G');
+          assign_case_cnd(G, 'G', true);
           assign_case(F, 'F');
           assign_case(P, 'P');
           assign_case(M, 'M');
@@ -526,8 +598,9 @@ break;
               options.out() << ss.str();
             }
           }
+          break;
         }
-      }      
+      }
     }
 };
 
@@ -577,4 +650,3 @@ int main(int argc, char *argv[]){
   
   return 0;
 }
-
