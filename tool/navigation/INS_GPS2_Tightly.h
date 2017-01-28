d968d5ce552d4dcadf40cfb5ed1c2c364c3f593c
fenrir <fenrir.naru@gmail.com>
2017-01-28 13:33:04 +0900

    Add filtered version for tightly-coupled.

--- tool/navigation/INS_GPS2_Tightly.h
+++ tool/navigation/INS_GPS2_Tightly.h
@@ -21,6 +21,7 @@
 
 #include "INS.h"
 #include "Filtered_INS2.h"
+#include "INS_GPS2.h"
 #include "GPS_SP.h"
 
 template <
@@ -114,5 +115,153 @@ template <class BaseINS>
 const unsigned Filtered_INS2_Property<INS_ClockErrorEstimated<BaseINS> >::Q_SIZE
     = Q_SIZE_WITHOUT_CLOCK_ERROR + INS_ClockErrorEstimated<BaseINS>::STATE_VALUES_CLOCK_ERROR;
 #endif
+
+template <
+    class BaseFINS = Filtered_INS2<INS_ClockErrorEstimated<INS<> > > >
+class Filtered_INS_ClockErrorEstimated : public BaseFINS {
+  public:
+#if defined(__GNUC__) && (__GNUC__ < 5)
+    typedef typename BaseFINS::float_t float_t;
+    typedef typename BaseFINS::vec3_t vec3_t;
+    typedef typename BaseFINS::mat_t mat_t;
+#else
+    using typename BaseFINS::float_t;
+    using typename BaseFINS::vec3_t;
+    using typename BaseFINS::mat_t;
+#endif
+
+  protected:
+    float_t m_beta_clock_error;
+
+  public:
+    using BaseFINS::ins_t::STATE_VALUES_WITHOUT_CLOCK_ERROR;
+    using BaseFINS::ins_t::STATE_VALUES_CLOCK_ERROR;
+    using BaseFINS::property_t::P_SIZE_WITHOUT_CLOCK_ERROR;
+    using BaseFINS::property_t::Q_SIZE_WITHOUT_CLOCK_ERROR;
+    using BaseFINS::m_clock_error;
+
+    Filtered_INS_ClockErrorEstimated()
+        : BaseFINS(),
+        m_beta_clock_error(1){
+
+    }
+
+    /**
+     * コピーコンストラクタ
+     *
+     * @param orig コピー元
+     * @param deepcopy ディープコピーを作成するかどうか
+     */
+    Filtered_INS_ClockErrorEstimated(
+        const Filtered_INS_ClockErrorEstimated &orig,
+        const bool deepcopy = false) :
+      BaseFINS(orig, deepcopy),
+      m_beta_clock_error(orig.m_beta_clock_error){
+
+    }
+
+    ~Filtered_INS_ClockErrorEstimated(){}
+
+    float_t &beta_clock_error(){return m_beta_clock_error;}
+
+    void getAB(
+        const vec3_t &accel,
+        const vec3_t &gyro,
+        typename BaseFINS::getAB_res &res) const {
+
+      BaseFINS::getAB(accel, gyro, res);
+
+      { // A行列の修正
+        // B行列の加速度、角速度に関する項をA行列へ
+        for(unsigned i(0); i < P_SIZE_WITHOUT_CLOCK_ERROR; i++){
+          for(unsigned j(P_SIZE_WITHOUT_CLOCK_ERROR), k(0); k < STATE_VALUES_CLOCK_ERROR; j++, k++){
+            res.A[i][j] = res.B[i][k];
+          }
+        }
+        // A行列のバイアスに関する対角成分を埋める
+        for(unsigned i(P_SIZE_WITHOUT_CLOCK_ERROR), j(0); j < STATE_VALUES_CLOCK_ERROR; i++, j++){
+          res.A[i][i] += -m_beta_clock_error;
+        }
+      }
+
+      { // B行列の修正
+        for(unsigned i(P_SIZE_WITHOUT_CLOCK_ERROR), j(Q_SIZE_WITHOUT_CLOCK_ERROR), k(0);
+             k < STATE_VALUES_CLOCK_ERROR;
+             i++, j++, k++){
+          res.B[i][j] += 1;
+        }
+      }
+    }
+
+    /**
+     * 時間更新
+     *
+     * @param accel 加速度計の値
+     * @param gyro ジャイロの値
+     * @param deltaT 時間間隔
+     */
+    void update(const vec3_t &accel, const vec3_t &gyro, const float_t &deltaT){
+      BaseFINS::update(accel, gyro, deltaT);
+    }
+
+    /**
+     * Kalman Filterによって得られた@f$ \Hat{x} @f$を利用して、INSを修正します。
+     *
+     * @param x_hat Kalman Filterによって得られたx_hat
+     */
+    void correct_INS(mat_t &x_hat){
+      for(unsigned i(P_SIZE_WITHOUT_CLOCK_ERROR), j(STATE_VALUES_WITHOUT_CLOCK_ERROR), k(0);
+          k < STATE_VALUES_CLOCK_ERROR;
+          i++, j++, k++){
+        (*this)[j] -= x_hat(i, 0);
+      }
+      BaseFINS::correct_INS(x_hat);
+    }
+
+    /**
+     * 修正します。
+     *
+     * @param H 観測行列
+     * @param z 観測量
+     * @param R 誤差共分散行列
+     */
+    void correct_primitive(const mat_t &H, const mat_t &z, const mat_t &R){
+      BaseFINS::correct_primitive(H, z, R);
+    }
+};
+
+/**
+ * @brief Tightly coupled INS/GPS
+ *
+ * Tightly coupled INS/GPS
+ *
+ * @param FloatT precision
+ * @param Filter type of Kalman filter
+ * @param BaseFINS Filtered INS, which mainly supports for time update procedure.
+ * @see Filtered_INS2
+ */
+template <
+  class FloatT,
+  template <class> class Filter = KalmanFilterUD,
+  typename BaseFINS = Filtered_INS_ClockErrorEstimated<
+      Filtered_INS2<INS_ClockErrorEstimated<INS<> >, Filter> >
+>
+class INS_GPS2_Tightly : public BaseFINS{
+  public:
+    INS_GPS2_Tightly() : BaseFINS(){} /**< コンストラクタ */
+
+    /**
+     * コピーコンストラクタ
+     *
+     * @param orig コピー元
+     * @param deepcopy ディープコピーを作成するかどうか
+     */
+    INS_GPS2_Tightly(const INS_GPS2_Tightly &orig, const bool deepcopy = false) :
+      BaseFINS(orig, deepcopy){
+
+    }
+
+    ~INS_GPS2_Tightly(){}         /**< デストラクタ */
+};
 
 #endif /* __INS_GPS2_TIGHTLY_H__ */
