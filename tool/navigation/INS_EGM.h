cf425f449948e69f377eca417042d38d3b1e2394
fenrir <fenrir.naru@gmail.com>
2017-10-15 23:12:35 +0900

    Add gravity calculation with EGM for INS

--- tool/navigation/INS_EGM.h
+++ tool/navigation/INS_EGM.h
@@ -41,8 +41,10 @@ class INS_EGM : public INS<FloatT> {
   public:
     typedef INS<FloatT> super_t;
 #if defined(__GNUC__) && (__GNUC__ < 5)
+    typedef typename super_t::float_t float_t;
     typedef typename super_t::vec3_t vec3_t;
 #else
+    using typename super_t::float_t;
     using typename super_t::vec3_t;
 #endif
   public:
@@ -77,9 +79,25 @@ class INS_EGM : public INS<FloatT> {
      * @return (vec3_t) total gravity in the navigation frame
      */
     virtual vec3_t gravity_total() const {
-      vec3_t res(0, 0, super_t::Earth::gravity(super_t::phi, super_t::h));
-      //EGM::gravity_r();
-      //res -= super_t::gravity_rotational();
+      typename super_t::Earth::xz_t xz(super_t::Earth::xz(super_t::phi, super_t::h));
+      float_t phi_gc(xz.geocentric_latitude()), r(xz.distance());
+      typename EGM::gravity_res_t g(EGM::gravity(r, phi_gc, super_t::lambda));
+
+      /* gravity_res_t is in a local frame, whose -Z direction points to the center,
+       * and whose X axis is parallel to meridian.
+       * Therefore, In order to coincide to n-frame, rotating delta-phi around the Y axis,
+       * then, rotating alpha around the Z axis are required.
+       */
+      float_t delta_phi(super_t::phi - phi_gc);
+      float_t cp(std::cos(delta_phi)), sp(std::sin(delta_phi));
+      float_t ca(std::cos(super_t::alpha)), sa(std::sin(super_t::alpha));
+
+      vec3_t res(
+          (g.phi *  ca * cp) + (g.lambda * sa) + (-g.r *  ca * sp),
+          (g.phi * -sa * cp) + (g.lambda * ca) + (-g.r * -sa * sp),
+          (g.phi       * sp)                   + (-g.r       * cp));
+
+      res -= super_t::gravity_rotational();
 
       return res;
     }
