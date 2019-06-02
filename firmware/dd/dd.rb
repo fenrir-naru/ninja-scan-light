f4792d208070824ac92d13015c46ec6a12efc84b
fenrir <fenrir.naru@gmail.com>
2019-06-02 21:44:28 +0900

    Improve stability and functionality of dd.rb
    * Add serialport::read_timeout
    * Add sectors option to calculate end_sector automatically

--- firmware/dd/dd.rb
+++ firmware/dd/dd.rb
@@ -7,18 +7,36 @@ opt = {
     :bps => 9600,
     :nbits => 8,
     :stopb => 1,
+    :parity => SerialPort::NONE,
   },
   :begin_sector => 0,
-  :end_sector => 1024 * 1024, # 512MB
+  :sectors => 1024 * 1024, # 512MB
+  :end_sector => -1, # Auto
   :each_sectors => 0x100,
 }
 
-ARGV.reject!{|arg|
+if ARGV.size < 1
+  $stderr.puts "Usage: #{$0} Serial_port_number [options]"
+  exit(1)
+end
+
+# set port number
+opt[:port][:num] = proc{
+  res = ARGV[0]
+  begin
+    res = Integer(res)
+    res = "/dev/ttyS#{res}" if ((`uname -a` =~ /cygwin/i) rescue false)
+  rescue
+  end
+  res
+}.call
+
+ARGV[1..-1].each{|arg|
   next false if arg !~ /--([^=]+)=?/
   k, v = [$1.to_sym, $' == "" ? true : $']
   case opt[k]
   when Fixnum
-    v = Integer(v)
+    v = Integer(v) rescue eval(v)
   when true, false
     v = (v == "true") if v.kind_of?(String)
   end
@@ -26,29 +44,22 @@ ARGV.reject!{|arg|
   true
 }
 
-# set port number
-if ARGV.size < 1
-  $stderr.puts "Usage: ruby #{$0} num_port [options]"
-  exit(1)
-end
-opt[:port][:num] = ARGV[0].to_i - (((`uname -a` =~ /cygwin/i) rescue false) ? 1 : 0)
+opt[:port][:name] = proc{
+  next opt[:port][:num] unless opt[:port][:num].kind_of?(Integer)
+  RUBY_PLATFORM =~ /win32/ ? "\\\\.\\COM#{opt[:port][:num]}" : "tty#{opt[:port][:num]}"
+}.call
 
-def port_name(port_num)
-  RUBY_PLATFORM =~ /win32/ ? "COM#{port_num}" : "tty#{port_num}"
-end
+opt[:dst_fname] ||= File::basename("#{opt[:port][:name]}_#{Time::now.strftime("%Y%d%m_%H%M%S")}.bin")
 
-opt[:dst_fname] ||= "#{port_name(opt[:port][:num])}_#{Time::now.strftime("%Y%d%m_%H%M%S")}.bin"
-
-sp = SerialPort.new(*([:num, :bps, :nbits, :stopb].collect{|k| opt[:port][k]} + [SerialPort::NONE]))
-$stderr.puts "#{port_name(opt[:port][:num])} opened."
+sp = SerialPort.new(*([:num, :bps, :nbits, :stopb, :parity].collect{|k| opt[:port][k]}))
+sp.read_timeout = 100
+$stderr.puts "#{opt[:port][:name]} opened."
 
 read_prop = proc{|k|
   sp.write(k)
-  if sp.gets !~ /\d+/
-    $stderr.puts "Unknown property(#{k})!"
-    exit(-1)
-  end
-  $&.to_i
+  next $&.to_i if sp.gets =~ /\d+/
+  $stderr.puts "Unknown property(#{k})!"
+  exit(-1)
 }
 
 property = {
@@ -62,17 +73,22 @@ read_block = proc{|sector_start, sectors|
   sp.write([sector_start, sectors].join(' '))
   raise if sp.gets !~ /READ\((\d+),(\d+)\)/
   raise if ($1.to_i != sector_start) || ($2.to_i != sectors)
-  sp.read(property[:size] * sectors)
+  res = sp.read(property[:size] * sectors)
+  raise if res.size != (property[:size] * sectors)
+  res
 }
 
 $stderr.puts "Save to #{opt[:dst_fname]}."
 open(opt[:dst_fname], 'a+'){|dst|
-  bs, es = [opt[:begin_sector], [property[:count], opt[:end_sector]].min]
+  bs, es, sectors = [:begin_sector, :end_sector, :sectors].collect{|k| opt[k]}
+  es = bs + sectors if es < 0
+  es = [property[:count], es].min
   sectors = es - bs
-  (bs..es).step(opt[:each_sectors]){|i|
+  each_sectors = opt[:each_sectors]
+  (bs...es).step(each_sectors){|i|
     i_rel = i - bs
-    $stderr.puts "Reading #{sprintf("%8d (%8d of %8d, %5.1f%%)", i, i_rel, sectors, 100.0 * i_rel / sectors)} ..."
-    dst.write(read_block.call(i, opt[:each_sectors]))
+    $stderr.puts "Reading #{sprintf("%8d (%8d of %8d, %5.1f%%)", i, i_rel, sectors, 100.0 * i_rel / sectors)} #{Time::now.strftime("%T")} ..."
+    dst.write(read_block.call(i, [each_sectors, es - i].min))
   }
   $stderr.puts "done."
 }
