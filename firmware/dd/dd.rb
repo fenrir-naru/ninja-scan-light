dd0ecfb698e7c8b863af0670ac037a5b5c2491f0
fenrir <fenrir.naru@gmail.com>
2020-10-13 16:13:02 +0900

    Fix dd.rb to use binary mode for Windows Ruby

--- firmware/dd/dd.rb
+++ firmware/dd/dd.rb
@@ -104,7 +104,7 @@ read_block = proc{|sector_start, sectors|
 }
 
 $stderr.puts "Save to #{opt[:dst_fname]}."
-open(opt[:dst_fname], 'a+'){|dst|
+open(opt[:dst_fname], "a+" + (/mswin|mingw/ =~ RUBY_PLATFORM ? 'b' : '')){|dst|
   bs, es, sectors = [:begin_sector, :end_sector, :sectors].collect{|k| opt[k]}
   es = bs + sectors if es < 0
   es = [property[:count], es].min
