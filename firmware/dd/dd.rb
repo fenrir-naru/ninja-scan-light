2b7cdca45db43e8629094eb2b1044d43f1446032
fenrir <fenrir.naru@gmail.com>
2020-10-14 22:32:52 +0900

    Fix destination file name

--- firmware/dd/dd.rb
+++ firmware/dd/dd.rb
@@ -55,7 +55,7 @@ opt[:port][:name] = proc{
   RUBY_PLATFORM =~ /win32/ ? "\\\\.\\COM#{opt[:port][:num]}" : "tty#{opt[:port][:num]}"
 }.call
 
-opt[:dst_fname] ||= File::basename("#{opt[:port][:name]}_#{Time::now.strftime("%Y%d%m_%H%M%S")}.bin")
+opt[:dst_fname] ||= File::basename("#{opt[:port][:name]}_#{Time::now.strftime("%Y%m%d_%H%M%S")}.bin")
 
 open_proc = proc{
   io = SerialPort.new(*([:num, :bps, :nbits, :stopb, :parity].collect{|k| opt[:port][k]}))
