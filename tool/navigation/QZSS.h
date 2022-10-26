97592fafa55e80354c104cb9f67862e3fe107492
fenrir <fenrir.naru@gmail.com>
2022-10-26 23:47:50 +0900

    Refactor QZSS ephemeris and almanac to have the same structure of GPS

--- tool/navigation/QZSS.h
+++ tool/navigation/QZSS.h
@@ -40,23 +40,56 @@
 #include <limits>
 
 template <class FloatT>
-struct QZSS_LNAV_Ephemeris_Raw : public GPS_SpaceNode<FloatT>::Satellite::Ephemeris::raw_t {
-  typedef typename GPS_SpaceNode<FloatT>::Satellite::Ephemeris eph_t;
-  typedef typename eph_t::raw_t super_t;
+struct QZSS_SpaceNode {
+  struct SatelliteProperties {
+    struct Ephemeris {
+      typedef typename GPS_SpaceNode<FloatT>::SatelliteProperties::Ephemeris eph_gps_t;
+      struct raw_t : public eph_gps_t::raw_t {
+        typedef typename eph_gps_t::raw_t super_t;
 
-  operator eph_t() const {
-    eph_t res(super_t::operator eph_t());
-    res.fit_interval = super_t::fit_interval_flag
-        ? (std::numeric_limits<FloatT>::max)()
-        : (2 * 60 * 60); // Fit interval (ICD:4.1.2.4 Subframe 2); should be zero
-    return res;
+        operator eph_gps_t() const {
+          eph_gps_t res(super_t::operator eph_gps_t());
+          res.fit_interval = super_t::fit_interval_flag
+              ? (std::numeric_limits<FloatT>::max)()
+              : (2 * 60 * 60); // Fit interval (ICD:4.1.2.4 Subframe 2); should be zero
+          return res;
+        };
+
+        raw_t &operator=(const eph_gps_t &eph){
+          super_t::operator=(eph);
+          super_t::fit_interval_flag = (eph.fit_interval > 2 * 60 * 60);
+          return *this;
+        }
+      };
+    };
+    struct Almanac {
+      typedef typename GPS_SpaceNode<FloatT>::SatelliteProperties::Almanac alm_gps_t;
+      struct raw_t : public alm_gps_t::raw_t {
+        typedef typename alm_gps_t::raw_t super_t;
+        template <int PaddingBits_MSB, int PaddingBits_LSB, class InputT>
+        void update(const InputT *src){
+          super_t::template update<PaddingBits_MSB, PaddingBits_LSB, InputT>(src);
+          if((super_t::svid >= 1) && (super_t::svid <= 10)){
+            super_t::svid += 192;
+          }
+        }
+        template <int PaddingBits_MSB, int PaddingBits_LSB, class BufferT>
+        void dump(BufferT *dst, const typename GPS_SpaceNode<FloatT>::u8_t &sf = 4){
+          super_t::template dump<PaddingBits_MSB, PaddingBits_LSB, BufferT>(dst);
+          typedef typename GPS_SpaceNode<FloatT>::template BroadcastedMessage<
+              BufferT, (int)sizeof(BufferT) * CHAR_BIT - PaddingBits_MSB - PaddingBits_LSB, PaddingBits_MSB>
+              deparse_t;
+          if((super_t::svid >= 193) && (super_t::svid <= 202)){
+            deparse_t::subframe_id_set(dst, sf); // sf = 4 or 5
+            deparse_t::sv_page_id_set(dst, super_t::svid - 192); // [1, 10]
+          }else{
+            return;
+          }
+          deparse_t::data_id_set(dst, 3); // always 3 @see 4.1.2.6.2. QZS Almanac
+        }
+      };
+    };
   };
-
-  QZSS_LNAV_Ephemeris_Raw &operator=(const eph_t &eph){
-    super_t::operator=(eph);
-    super_t::fit_interval_flag = (eph.fit_interval > 2 * 60 * 60);
-    return *this;
-  }
 };
 
 #endif /* __QZSS_H__ */
